446fe09fa327ae7e8ba3a20494722794b53cf271
indiamaris <indiamaris@gmail.com>
2024-01-12 20:52:00 +0100

    o grafico que me fez chorar

---
+++ js/demo/demoniandia/consumers.js
@@ -0,0 +1,190 @@
+
+
+
+//fake data 
+
+var data  = [
+    {
+        month: 'jan', nested:
+            { count: 10, countB: 290, countD: 870, countE: 20, countF: 780, countG: 200, countH: 200, countI: 200, countJ: 200, countK: 200, countL: 200 }
+    },
+    {
+        month: 'fev', nested:
+            { count: 23, countB: 200, countD: 200, countE: 20, countF: 200, countG: 200, countH: 200, countI: 200, countJ: 60, countK: 200, countL: 200 }
+    },
+    {
+        month: 'mar', nested:
+            { count: 70, countB: 200, countD: 26, countE: 200, countF: 200, countG: 200, countH: 200, countI: 200, countJ: 20, countK: 200, countL: 200 }
+    },
+        {
+        month: 'apr', nested:
+            { count: 120, countB: 40, countD: 20, countE: 200, countF: 500, countG: 700, countH: 200, countI: 200, countJ: 70, countK: 600, countL: 800 }
+    },
+    {
+        month: 'jun', nested:
+            { count: 150, countB: 340, countD: 20, countE: 700, countF: 500, countG: 270, countH: 900, countI: 120, countJ: 20, countK: 260, countL: 987 }
+    },
+    {
+        month: 'july', nested:
+            { count: 40, countB: 670, countD: 950, countE: 900, countF: 1200, countG: 290, countH: 900, countI: 190, countJ: 760, countK: 260, countL: 890 }
+    },
+    {
+        month: 'aug', nested:
+            { count: 30, countB: 23, countD: 650, countE: 270, countF: 290, countG: 223, countH: 200, countI: 200, countJ: 890, countK: 200, countL: 600 }
+    },
+    {
+        month: 'sep', nested:
+            { count: 15, countB: 230, countD: 25, countE: 200, countF: 900, countG: 20, countH: 200, countI: 270, countJ: 200, countK: 340, countL: 500 }
+    },
+    {
+        month: 'oct', nested:
+            { count: 10, countB: 70, countD: 260, countE: 243, countF: 290, countG: 26, countH: 980, countI: 130, countJ: 20, countK: 90, countL: 700 }
+    },
+    {
+        month: 'nov', nested:
+            { count: 145, countB: 200, countD: 270, countE: 900, countF: 600, countG: 400, countH: 200, countI: 800, countJ: 290, countK: 200, countL: 900 }
+    },
+    {
+        month: 'dec', nested:
+            { count: 10, countB: 12, countD: 890, countE: 100, countF: 200, countG: 270, countH: 200, countI: 60, countJ: 50, countK: 20, countL: 90 }
+    },
+]
+  
+//fim fake data
+var ctx = document.getElementById('consumers')
+ 
+var consumers = new Chart(ctx,   {
+      type: 'bar',
+      data: {
+        labels: data.map(row => row.month), 
+        
+        datasets: [
+          {
+            label: 'Disney Orlando',
+                 backgroundColor: "#ffb800",
+      hoverBackgroundColor: "#ffb800",
+      borderColor: "#ffb800",
+             barThickness: 6
+             ,
+            data: data.map(row => row.nested.count)
+          },
+          {
+             backgroundColor: "red",
+      hoverBackgroundColor: "#red",
+      borderColor: "#red",
+            label: 'Disney France',
+             barThickness: 6
+             ,
+            data: data.map(row => row.nested.countB)
+          }
+          ,
+          {
+             backgroundColor: "#FFCCFF",
+      hoverBackgroundColor: "#2e59d9",
+      borderColor: "#FFCCFF",
+            label: 'Disney Itajai',
+             barThickness: 6
+             ,
+            data: data.map(row => row.nested.countC)
+          }
+          ,
+          {
+             backgroundColor: "#E5FFCC",
+      hoverBackgroundColor: "#2e59d9",
+      borderColor: "#E5FFCCf",
+            label: ' Beto Carreiro',
+             barThickness: 6
+             ,
+            data: data.map(row => row.nested.countD)
+          }
+          ,
+          {
+             backgroundColor: "#CCE5FF",
+      hoverBackgroundColor: "#CCFFFF",
+      borderColor: "#CCFFFF",
+            label: 'Loja do Zeh',
+             barThickness: 6
+             ,
+            data: data.map(row => row.nested.countE)
+          }
+          ,
+          {
+             backgroundColor: "#55ff00",
+      hoverBackgroundColor: "#2e59d9",
+      borderColor: "#55ff00",
+            label: 'BC Shopping',
+             barThickness: 6,
+            data: data.map(row => row.nested.countF)
+          }
+          ,
+          {
+             backgroundColor: "#ff00fc",
+      hoverBackgroundColor: "#2e59d9",
+      borderColor: "#ff00fc",
+            label: 'Camelo',
+             barThickness: 6
+             ,
+            data: data.map(row => row.nested.countG)
+          }
+          ,
+          {
+                backgroundColor: "#00ffa7",
+      hoverBackgroundColor: "#2e59d9",
+      borderColor: "#00ffa7",
+            label: 'Rocinha',
+             barThickness: 6
+             ,
+            data: data.map(row => row.nested.countH)
+          }
+          ,
+          {
+             backgroundColor: "#4e73df",
+      hoverBackgroundColor: "#2e59d9",
+      borderColor: "#4e73df",
+            label: 'Holywod',
+             barThickness: 6
+             ,
+            data: data.map(row => row.nested.countI)
+          }
+          ,
+          {
+             backgroundColor: "#00fffb",
+      hoverBackgroundColor: "#2e59d9",
+      borderColor: "#00fffb",
+            label: 'casa de lares',
+             barThickness: 6
+             ,
+            data: data.map(row => row.nested.countJ)
+          }
+           ,
+          {
+             backgroundColor: "#c4ff00",
+      hoverBackgroundColor: "#2e59d9",
+      borderColor: "#c4ff00",
+            label: 'Indialandia',
+             barThickness: 6
+             ,
+            data: data.map(row => row.nested.countH)
+          },
+          {
+             backgroundColor: "#fffc00",
+      hoverBackgroundColor: "#2e59d9",
+      borderColor: "#fffc00",
+            label: 'Ali do lado',
+             barThickness: 6
+             ,
+            data: data.map(row => row.nested.countK)
+          },
+          {
+             backgroundColor: "#ff007a",
+      hoverBackgroundColor: "#2e59d9",
+      borderColor: "#ff007a",
+            label: 'Adilsonlandia',
+             barThickness: 6
+             ,
+            data: data.map(row => row.nested.countL)
+          }
+        ]
+      }
+    })
+ 
---
+++ js/demo/demoniandia/data.js
@@ -0,0 +1,46 @@
+// var data  = [
+//     {
+//         month: 'jan', nested:
+//             { count: 10, countB: 290, countD: 870, countE: 20, countF: 780, countG: 200, countH: 200, countI: 200, countJ: 200, countK: 200, countL: 200 }
+//     },
+//     {
+//         month: 'fev', nested:
+//             { count: 23, countB: 200, countD: 200, countE: 20, countF: 200, countG: 200, countH: 200, countI: 200, countJ: 60, countK: 200, countL: 200 }
+//     },
+//     {
+//         month: 'mar', nested:
+//             { count: 70, countB: 200, countD: 26, countE: 200, countF: 200, countG: 200, countH: 200, countI: 200, countJ: 20, countK: 200, countL: 200 }
+//     },
+//         {
+//         month: 'apr', nested:
+//             { count: 120, countB: 40, countD: 20, countE: 200, countF: 500, countG: 700, countH: 200, countI: 200, countJ: 70, countK: 600, countL: 800 }
+//     },
+//     {
+//         month: 'jun', nested:
+//             { count: 150, countB: 340, countD: 20, countE: 700, countF: 500, countG: 270, countH: 900, countI: 120, countJ: 20, countK: 260, countL: 987 }
+//     },
+//     {
+//         month: 'july', nested:
+//             { count: 40, countB: 670, countD: 950, countE: 900, countF: 1200, countG: 290, countH: 900, countI: 190, countJ: 760, countK: 260, countL: 890 }
+//     },
+//     {
+//         month: 'aug', nested:
+//             { count: 30, countB: 23, countD: 650, countE: 270, countF: 290, countG: 223, countH: 200, countI: 200, countJ: 890, countK: 200, countL: 600 }
+//     },
+//     {
+//         month: 'sep', nested:
+//             { count: 15, countB: 230, countD: 25, countE: 200, countF: 900, countG: 20, countH: 200, countI: 270, countJ: 200, countK: 340, countL: 500 }
+//     },
+//     {
+//         month: 'oct', nested:
+//             { count: 10, countB: 70, countD: 260, countE: 243, countF: 290, countG: 26, countH: 980, countI: 130, countJ: 20, countK: 90, countL: 700 }
+//     },
+//     {
+//         month: 'nov', nested:
+//             { count: 145, countB: 200, countD: 270, countE: 900, countF: 600, countG: 400, countH: 200, countI: 800, countJ: 290, countK: 200, countL: 900 }
+//     },
+//     {
+//         month: 'dec', nested:
+//             { count: 10, countB: 12, countD: 890, countE: 100, countF: 200, countG: 270, countH: 200, countI: 60, countJ: 50, countK: 20, countL: 90 }
+//     },
+//   ]
